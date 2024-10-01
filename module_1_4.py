65f12087be3820bf860a0118e497b33f9c208b17
IvanSamdi <ivan.malin2014@gmail.com>
2024-10-01 13:51:04 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,6 @@
+number_of_completed_HA = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_completed_HA
+print('Курс:', course_name, 'Всего задач:', number_of_completed_HA, 'Затрачено часов:', number_of_hours_spent,
+      'Среднее время выполнения:', time_for_one_task)
